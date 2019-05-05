3f01fd1cb94bc06d836d57ec6dd38e8a376e8d71
AvdoninPav <onlytarg@gmail.com>
2019-05-05 14:12:30 +0300

    add writeToFile method

--- src/main/java/IO/WriteFileToFile.java
+++ src/main/java/IO/WriteFileToFile.java
@@ -1,4 +1,27 @@
 package IO;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+
 public class WriteFileToFile {
+    public static String SOURCEFILE = "D:/1.txt";
+    public static String TARGETFILE = "D:/2.txt";
+
+    public static void writeFile(File sourceFile, File targetFile) throws IOException {
+        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(sourceFile));
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(targetFile));
+        while (inputStream.available() != 0) {
+
+            outputStream.write(inputStream.read());
+        }
+        inputStream.close();
+        outputStream.close();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        WriteFileToFile.writeFile(new File(SOURCEFILE), new File(TARGETFILE));
+    }
+
 }
